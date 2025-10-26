8c744724e8c4fd50300c0e5bb4f2462ba6f37a5f
Dave Bernier <bernier.db@gmail.com>
2025-10-26 11:27:43 -0400

    refactor: enhance Tool Use Workflow by encapsulating LLM and adding public property

--- projects/05_Tool_Use/main.py
+++ projects/05_Tool_Use/main.py
@@ -6,6 +6,7 @@ This workflow demonstrates the Tool Use agentic design pattern using langchain.
 
 import json
 from langchain_openai import ChatOpenAI
+from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import (
     SystemMessage,
     HumanMessage,
@@ -22,25 +23,30 @@ class ToolUseWorkflow:
     """Tool Use Workflow"""
 
     _conversation_messages: list[BaseMessage] = []
+    _llm: BaseChatModel
+    publicProperty = "public property"
 
     def __init__(self):
         """Initialize the workflow with LLM configuration."""
+
         try:
-            self.llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
-            self.llm = self.llm.bind_tools([search_database])
+            self._llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
+            self._llm = self._llm.bind_tools([search_database])
+
         except Exception as e:
             print(f"Error initializing LLM: {e}")
             exit(1)
 
     def run(self):
         """Run the workflow."""
+
         print("--- Running Tool Use Workflow ---\n")
 
         result = self._start_conversation()
 
         self._handle_tools_calls(result.tool_calls)
 
-        final_response_message = self.llm.invoke(self._conversation_messages)
+        final_response_message = self._llm.invoke(self._conversation_messages)
 
         final_response = final_response_message.content
 
@@ -49,6 +55,7 @@ class ToolUseWorkflow:
 
     def _start_conversation(self):
         """Start the conversation."""
+
         self._conversation_messages.append(
             SystemMessage(
                 content="You are an assistant that can search a database of books."
@@ -59,17 +66,19 @@ class ToolUseWorkflow:
                 content="How many copies of the book 'The Great Gatsby' are in the database?"
             )
         )
-        result = self.llm.invoke(self._conversation_messages)
+        result = self._llm.invoke(self._conversation_messages)
         self._conversation_messages.append(result)
         return result
 
     def _handle_tools_calls(self, tool_calls: list[dict]):
         """Handle tool calls."""
+
         for tool_call in tool_calls:
             self._handle_tool_call(tool_call)
 
     def _handle_tool_call(self, tool_call: dict):
         """Handle a tool call."""
+
         tool_name = tool_call["name"]
         tool_args = tool_call["args"]
         if tool_name == "search_database":
